1cd87f4f12dc0efc88c96a1fdf4463f8f367acf4
Re <reaperofblack@gmail.com>
2022-11-24 12:29:15 +0800

    fixed script not working on latest Windows 11 / WSL

--- run.cmd
+++ run.cmd
@@ -1,3 +1,5 @@
 echo off
 
-clear && python -m mkdocs serve -a 0.0.0.0:8000
+clear
+
+python -m mkdocs serve -a 0.0.0.0:8000
--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/sh
 
-clear && python -m mkdocs serve -a 0.0.0.0:8000
+clear
+
+python -m mkdocs serve -a 0.0.0.0:8000
